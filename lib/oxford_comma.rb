4dbdaaffeda622731eb2b7aee3d296ce14f5bc13
Matt <ehxtwo@gmail.com>
2021-02-24 19:40:05 -0800

    commit tree 6f38cdd048b22622d590cccf54c692e67b02b3d6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        array.join(" and ")
+    else 
+        x = array.size - 1
+        front = array[0,x].join(", ")
+        front << ", and #{array.last}"
+    end
 end
